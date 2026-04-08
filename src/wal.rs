0d5aaa9374b8a01badb3556a33996238f8f6c478
libinjareo <libinjareo@163.com>
2026-04-08 17:51:53 +0800

    feat:完成wal的create,recover,put,sync方法

--- src/wal.rs
+++ src/wal.rs
@@ -1,9 +1,13 @@
+use crate::key::{KeyBytes, KeySlice};
 use anyhow::{bail, Context, Result};
+use bytes::{Buf, BufMut, Bytes};
+use crossbeam_skiplist::SkipMap;
 use parking_lot::Mutex;
 use std::fs::{File, OpenOptions};
-use std::io::BufWriter;
-use std::sync::Arc;
+use std::hash::Hasher;
+use std::io::{BufWriter, Read, Write};
 use std::path::Path;
+use std::sync::Arc;
 
 pub struct Wal {
     /*
@@ -54,7 +58,113 @@ impl Wal {
         })
     }
 
-    // pub fn put(&self,key:KeySlice,value:&[u8]) -> Result<()> {
-    //     todo!()
-    // }
+    // 从已有的WAL文件中读取所有记录，逐条验证校验和，并将有效的键值对插入到内存跳表 skiplist 中。
+    // 最后返回一个新的WAL示例，用于后续继续追加写入
+    pub fn recover(path: impl AsRef<Path>, skiplist: &SkipMap<KeyBytes, Bytes>) -> Result<Self> {
+        let path = path.as_ref();
+        let mut file = OpenOptions::new()
+            .read(true)
+            .append(true) // 以追加的模式打开现有文件
+            .open(path)
+            .context("failed to recover WAL")?;
+        // 一次性读取所有数据到Vec<u8>，然后使用&[u8]作为游标进行解析
+        // 但是对于巨大文件可能消耗较多的内存，此处为了简洁，采用全量读取；
+        // 可以使用BufReader流式解析进行优化
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf)?;
+        let mut rbuf = buf.as_slice(); // 将整个文件读入内存
+        while rbuf.has_remaining() { // 只要还有未读字节就继续
+            let mut hasher = crc32fast::Hasher::new();
+
+            let key_len = rbuf.get_u16() as usize; // 从缓冲区读取2字节并前进游标
+            hasher.write_u16(key_len as u16); // 开始计算校验和，每次解析一个字段前，先更新 hasher
+
+            // 会分配新的内存，将key字节复制出来，然后更新哈西并移动游标
+            let key = Bytes::copy_from_slice(&rbuf[..key_len]);
+            hasher.write(&key);
+            rbuf.advance(key_len);
+
+            // 读取ts
+            let ts = rbuf.get_u64();
+            hasher.write_u64(ts);
+
+            // 读取 value_len
+            let value_len = rbuf.get_u16() as usize;
+            hasher.write_u16(value_len as u16);
+
+            // 读取 value 内容
+            let value = Bytes::copy_from_slice(&rbuf[..value_len]);
+            hasher.write(&value);
+            rbuf.advance(value_len);
+
+            // 读取存储的  checkSum
+            let checksum = rbuf.get_u32();
+
+            // 检验
+            if hasher.finalize() != checksum {
+                bail!("checksum mismatch");  //防止使用损坏的或者不完整的 WAL 文件
+            }
+
+            // 插入调表，会把时间戳编码进 key 的内部表示（通常用于 MVCC 或版本管理）
+            skiplist.insert(KeyBytes::from_bytes_with_ts(key, ts), value);
+        }
+
+        Ok(Self {
+            // 此处复用了之前的file对象（它已经以 append 模式打开，且文件位置已经在末尾），然后创建BufWriter
+            // 之后调用 put 时，新记录会追加到原有数据之后
+            file: Arc::new(Mutex::new(BufWriter::new(file))),
+        })
+    }
+
+    // 将一条健值对记录（附带key的时间戳）追加写入WAL文件，同时计算并写入CRC32校验和，用于后续恢复时检测数据完整性
+    pub fn put(&self, key: KeySlice, value: &[u8]) -> Result<()> {
+        // 需要先上锁，获得独占访问权。锁会再函数返回时自动释放
+        let mut file = self.file.lock();
+        // 预分配内存缓冲区
+        // 预分配足够的容量，避免写入过程中反复扩容。
+        // key内容长度+时间戳(u64) + value长度 + 2字节（size_of::<u16>）
+        let mut buf: Vec<u8> = Vec::with_capacity(key.raw_len() + value.len() + std::mem::size_of::<u16>());
+        // 用于计算记录中所有字段的校验和
+        let mut hasher = crc32fast::Hasher::new();
+        //按固定格式写入数据并更新哈希
+
+        // Key内容的字节长度（不含时间戳），字节数：2
+        hasher.write_u16(key.key_len() as u16); // 更新校验和
+        buf.put_u16(key.key_len() as u16); // 将数据写入缓冲区
+
+        // key的实际内容，字节数:key_len
+        hasher.write(key.key_ref()); // 更新校验和
+        buf.put_slice(key.key_ref()); // 将数据写入缓冲区
+
+        // 时间戳（用于版本/排序），字节数：8
+        hasher.write_u64(key.ts()); // 更新校验和
+        buf.put_u64(key.ts()); // 将数据写入缓冲区
+
+        // value内容的字节长度，字节数:2
+        hasher.write_u16(value.len() as u16); // 更新校验和
+        buf.put_u16(value.len() as u16); // 将数据写入缓冲区
+
+        // value的实际数据，字节数：value_len
+        buf.put_slice(value); // 更新校验和
+        hasher.write(value); // 将数据写入缓冲区
+
+        // todo add checksum
+
+        // 从哈希器中得到最终的CRC32校验和，追加到缓冲区末尾,字节数:4
+        buf.put_u32(hasher.finalize());
+
+        // 因file 是BufWriter<File>，数据会先进入BufWriter的内部缓冲区，未必立即落盘
+        file.write_all(&buf)?;
+
+        // 方法没有调用flush 或 sync，因此写入的数据可能还在内存缓冲区中。
+        // 显式的持久化由sync()方法完成，这符合WAL的常见设计：允许批量吸入后统一同步，提高性能
+        Ok(())
+    }
+
+    pub fn sync(&self) -> Result<()> {
+        let mut file = self.file.lock();
+        file.flush()?;
+        file.get_mut().sync_all()?;
+        Ok(())
+    }
 }
